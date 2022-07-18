e5be04c8b51e9c06423e4c885e73f655504891b0
Jorge Cuevas <jorge@Logicals-Mac-mini.local>
2022-07-17 19:20:58 -0500

    Add: helm

--- personal/preload/config-multiple-cursors.el
+++ personal/preload/config-multiple-cursors.el
@@ -1,5 +1,5 @@
 ;; Multiple cursors
-(require 'multiple-cursors)
+'(require multiple-cursors nil t)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -41,13 +41,13 @@
 
 ;;; General productivity tools
 
-;; (require 'prelude-ido) ;; Supercharges Emacs completion for C-x C-f and more
+(require 'prelude-ido) ;; Supercharges Emacs completion for C-x C-f and more
 (require 'prelude-ivy) ;; A mighty modern alternative to ido
-;; (require 'prelude-selectrum) ;; A powerful, yet simple, alternative to ivy
+(require 'prelude-selectrum) ;; A powerful, yet simple, alternative to ivy
 (require 'prelude-helm) ;; Interface for narrowing and search
 (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
-;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
+(require 'prelude-key-chord) ;; Binds useful features to key combinations
 
 ;;; Vim emulation
 ;;
@@ -66,7 +66,7 @@
 
 (require 'prelude-c)
 ;; (require 'prelude-clojure)
-(require 'prelude-coffee)
+;; (require 'prelude-coffee)
 ;; (require 'prelude-common-lisp)
 (require 'prelude-css)
 ;; (require 'prelude-dart)
@@ -84,14 +84,14 @@
 ;; (require 'prelude-lua)
 ;; (require 'prelude-ocaml)
 (require 'prelude-perl)
-(require 'prelude-python)
+;; (require 'prelude-python)
 ;; (require 'prelude-racket)
-(require 'prelude-ruby)
+;; (require 'prelude-ruby)
 ;; (require 'prelude-rust)
 ;; (require 'prelude-scala)
 ;; (require 'prelude-scheme)
 (require 'prelude-shell)
-(require 'prelude-scss)
+;; (require 'prelude-scss)
 ;; (require 'prelude-ts)
 (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
